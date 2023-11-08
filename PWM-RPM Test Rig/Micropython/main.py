46840d6b8cd7d3d63d5540c8608c148dc8f49b4c
sccleme1 <31865398+s-c-clemens@users.noreply.github.com>
2023-11-07 17:13:32 -0700

    Update main.py

--- PWM-RPM Test Rig/Micropython/main.py
+++ PWM-RPM Test Rig/Micropython/main.py
@@ -18,25 +18,25 @@ hits = 0
 nohits = 0
 
 
-pwm_motor.duty_u16(3180)
-sleep(2) # wait 1.25 seconds to get up to speed
-
-for i in range(100000):
-  if IR_pin.value() == 1:
-      hits += 1
-      #print("Hit")
-  else:
-      nohits += 1
-      #print("No Hit")
-  sleep(0.00001)
-  
-pwm_motor.duty_u16(0) # turn off
-
-print(f"At 10 us polling:")
-print(f"Hits: {hits} \t No Hits: {nohits}")
-print(f"Total hit percentage: {100*hits/(nohits+hits)}%")
-print(f"Estimated RPMs: {hits*60/8}")
-print()
+# pwm_motor.duty_u16(3180)
+# sleep(2) # wait 1.25 seconds to get up to speed
+# 
+# for i in range(100000):
+#   if IR_pin.value() == 1:
+#       hits += 1
+#       #print("Hit")
+#   else:
+#       nohits += 1
+#       #print("No Hit")
+#   sleep(0.00001)
+#   
+# pwm_motor.duty_u16(0) # turn off
+# 
+# print(f"At 10 us polling:")
+# print(f"Hits: {hits} \t No Hits: {nohits}")
+# print(f"Total hit percentage: {100*hits/(nohits+hits)}%")
+# print(f"Estimated RPMs: {hits*60/8}")
+# print()
 
 # sleep(10)
 # 
@@ -60,20 +60,39 @@ print()
 # print(f"Hits: {hits} \t No Hits: {nohits}")
 # print(f"Total hit percentage: {100*hits/(nohits+hits)}%")
 # print(f"Estimated RPMs: {hits*60}")
+sleep_time = 1
+while True:
 
-# while True:
-# 
 #     for duty in range(6):
 #          pwm_built_in.duty_u16(duty*1200)
 #          pwm_motor.duty_u16(10*duty + 3150)
 #          print(f"PWM = {10*duty + 3150}")
 #          sleep(1)
-#      
-#      pwm_motor.duty_u16(0)
-#      pwm_motor.duty_u16(3200) # 3180
-#      sleep(4)
-#      pwm_motor.duty_u16(0)
-#      sleep(10)
+     
+     #pwm_motor.duty_u16(0)
+    pwm_motor.duty_u16(3180) # 3180
+    sleep(sleep_time)
+     
+    for i in range(100000):
+        if IR_pin.value() == 1:
+            hits += 1
+              #print("Hit")
+        else:
+            nohits += 1
+              #print("No Hit")
+        sleep(0.00001)
+          
+    pwm_motor.duty_u16(0) # turn off
+
+    print(f"At 10 us polling with drive time {sleep_time} s:")
+    print(f"Hits: {hits} \t No Hits: {nohits}")
+    print(f"Total hit percentage: {100*hits/(nohits+hits)}%")
+    print(f"Estimated RPMs: {hits*60/8} RPMs")
+    print()
+    hits = 0
+    nohits = 0
+    sleep_time += 1
+    sleep(10)
      
 #     for duty in range(5, 0, -1):
 #          pwm_built_in.duty_u16(duty*1200)
